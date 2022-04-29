770189f0cc94ca95fbb28abef93c2fdd30c630e3
Kaanayden <kaanayden15@gmail.com>
2022-04-29 23:46:22 +0300

    Updates

--- Data Generator/RecipeByteGenerator.java
+++ Data Generator/RecipeByteGenerator.java
@@ -1,10 +1,20 @@
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.Scanner;
 public class RecipeByteGenerator {
 
-    public static String fillWithZeros( String text, int length ) {
+    enum Direction {
+        LEFT,
+        RIGHT
+    }
+
+    public static String fillWithZeros( String text, int length, Direction direction ) {
         String result = text;
-        while( text.length() < length ) {
-            result += "0";
+        while( result.length() < length ) {
+            if( direction == Direction.RIGHT )
+            result = result + "0"; 
+            else 
+            result = "0" + result;
         }
         return result;
     }
@@ -17,17 +27,46 @@ public class RecipeByteGenerator {
             number /= 2;
         }
 
-        result = fillWithZeros( result , length);
+        result = fillWithZeros( result , length, Direction.LEFT);
         return result;
     }
-    public static void main( String[] args ) {
-        String bytesString = "";
-        Scanner input = new Scanner( System.in );
 
-        System.out.println( convertToBits(5, 7) );
+    public static char convertDecimalToHex( int decimal ) {
+        if( decimal > 9 ) {
+            return (char) ( decimal + 87 );
+        } else {
+            return (char) (decimal + 48);
+        }
+    }
+
+    public static String convertTo4BytesHex( String bits ) {
+        String hexCode = "";
+        for( int i = 0; i < 8; i++ ) {
+            String currentPart = bits.substring(i * 4, i * 4 + 4);
+            int decimal = 0;
+            bits = bits.substring( i * 4 + 4 );
+
+            for( int k = 8; k != 0; k /= 2) {
+                if( bits.charAt( 0 ) == '1' ) {
+                    decimal += k;
+                }
+            }
+            hexCode += convertDecimalToHex( decimal );
+
+        }
+        return hexCode;
+    }
+    public static void main( String[] args ) throws FileNotFoundException {
+        String bytesString = "";
+        final String TEXT_FILE_NAME = "Recipes.txt";
+        System.out.println("Started...");
+        File textFile = new File( TEXT_FILE_NAME );
+        Scanner input = new Scanner( textFile );
+
 
         while( input.hasNext() ) {
             String bits = "";
+            String hexCode = "0x";
             String current = input.next();
             if( !current.startsWith("//") ) {
                 if( current.equals( "true" ) ) { // is craftable
@@ -43,8 +82,8 @@ public class RecipeByteGenerator {
                         bits += convertToBits( itemNumber, 5 ); // item index
                         bits += convertToBits( itemNumber, 3 ); // item amount
                     }
-
-
+                    bits = fillWithZeros( bits , 8 * 4, Direction.RIGHT);
+                    hexCode += convertTo4BytesHex(bits);
                 }
                 else {
                     bits += "0";
--- Data Generator/Test2.java
+++
@@ -1,9 +0,0 @@
-
-
-public class Test2 {
-
-    public static void main( String[] args ) {
-        RecipeByteGenerator.convertToBits(5,3);
-    }
-    
-}
